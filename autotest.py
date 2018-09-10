65152ded64c058ab5a664554984af3306f297871
L3viathan <git@l3vi.de>
2018-09-10 22:09:01 +0300

    Allow direct writing of file
    Overwriting builtins.print and using open without context managers,
    nice.

--- autotest.py
+++ autotest.py
@@ -65,13 +65,24 @@ def print_tests(fn, mod):
 
 @click.command()
 @click.argument("filename", type=click.Path())
-def cli(filename):
-    m = __import__(filename[:-3])
-    print(f"import pytest\n\nimport {filename[:-3]}")
+@click.option("--write-file", is_flag=True)
+def cli(filename, write_file):
+    mod = filename[:-3]
+    if write_file:
+        fp = open(f"test_{mod}.py", "w")
+        def print(*args):
+            fp.write(" ".join(args))
+            fp.write("\n")
+        import builtins
+        builtins.print = print
+    m = __import__(mod)
+    print(f"import pytest\n\nimport {mod}")
     for member in dir(m):
         thing = getattr(m, member)
         if inspect.isfunction(thing):
-            print_tests(thing, filename[:-3])
+            print_tests(thing, mod)
+    if write_file:
+        fp.close()
 
 if __name__ == '__main__':
     cli()  # pytest: disable=no-value-for-parameter
